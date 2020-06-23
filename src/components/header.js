f310f697812802fa5e49a501b86741579c018d17
SyedAli00896 <SyedAli00896@gmail.com>
2020-06-23 20:11:55 +0500

    navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,77 @@
+import React, { Component } from "react"
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+import "../Styles/styles.css"
+class Header extends Component {
+  render() {
+    return (
+      <nav
+        class="navbar navbar-expand-lg navbar-collapse "
+        style={{
+          flexDirection: "row",
+          backgroundColor: "darkorange",
+        }}
+      >
+        <div
+          class="container"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            display: "flex",
+            flexDirection: "row",
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+              <img
+                src={require("../images/shrine_horizontal_white.png")}
+                width="156"
+                height="61"
+                alt="Shrine"
+              />
+            </a>
+          </div>
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <Link>
+                <a href="./" id="anchor">
+                  Services
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <a id="anchor" href="./">
+                  Portfolio
+                </a>
+              </Link>
+            </li>
 
-Header.defaultProps = {
-  siteTitle: ``,
+            <li>
+              <Link>
+                <a id="anchor" href="./">
+                  About
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <a id="anchor" href="./">
+                  Insights
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <a id="contact" href="./">
+                  Contact
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default Header
